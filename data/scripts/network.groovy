5631cc0bab2c3bfa2bbdd2a02da49d9fb8e03eaf
Harry Andreas <harry@runecore.org>
2013-02-08 14:35:14 +0000

    Ported network + context binding to groovy

---
+++ data/scripts/network.groovy
@@ -0,0 +1,10 @@
+class Network implements GroovyScript {
+
+	void init(Context context) {
+		def socket = new InetSocketAddress("0.0.0.0", 43594);
+		def network = new NetworkContext(socket);
+		network.configure(context);
+		network.bind();
+	}
+	
+}
