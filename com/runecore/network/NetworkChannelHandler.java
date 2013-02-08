e9a33d1dfb6ae63df3abea41186465114f2fe96d
Harry Andreas <harry@runecore.org>
2013-02-08 12:40:49 +0000

    NetworkChannelHandler changes

--- com/runecore/network/NetworkChannelHandler.java
+++ com/runecore/network/NetworkChannelHandler.java
@@ -1,5 +1,9 @@
 package com.runecore.network;
 
+import java.util.logging.Logger;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 
 /**
@@ -8,5 +12,17 @@ import org.jboss.netty.channel.SimpleChannelHandler;
  * Feb 8, 2013
  */
 public class NetworkChannelHandler extends SimpleChannelHandler {
-
+    
+    private static final Logger LOGGER = Logger.getLogger(NetworkChannelHandler.class.getName());
+    
+    @Override
+    public final void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) {
+	LOGGER.info("New connection from "+e.getChannel().getRemoteAddress().toString());
+    }
+    
+    @Override
+    public final void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) {
+	LOGGER.info("Channel ("+e.getChannel().getRemoteAddress().toString()+") closed");
+    }
+    
 }
